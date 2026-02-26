023fb35609f9235ef7c068f800e20b5285b95b15
DelphanPAN <89398821+dolbaebsans@users.noreply.github.com>
2026-02-26 12:24:03 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 // да бля ну нахуя тебе сюда😭😭😭😭 съеби нахуй пожалйсат💔
 
+let cool_list = {
+    test: ["cool_logo.png", "asdf.png", "cool_logo.png"],
+    минекрафт: ["https://drive.google.com/uc?export=download&id=1TuBXj7gHjbA6xENjWfwLdKYJaefypcAx", "минекрафт.rar", "apps/minecraft/icon.jpg"]
+}
+
 let vse = document.getElementById("hui")
+const container = document.getElementById("container")
 
 function sayHello() {
     vse.style.display = "none";
@@ -8,6 +14,45 @@ function sayHello() {
 
 // Assign the function to the window's onload event
 window.onload = sayHello; 
+
+function successful() {
+    alert("краава");
+
+    vse.style.display = "flex"
+
+    let clone_amount = 1
+
+    const template = document.getElementById("app_template");
+
+    for (let i = 0; (i < Object.keys(cool_list).length); i++) {
+        console.log(i, "list")
+
+        let clone = template.cloneNode(true)
+        let clone_app_name = clone.childNodes[3]
+        let clone_app_download = clone.childNodes[1]
+        let clone_app_img = clone_app_download.lastElementChild
+
+        console.log(clone.childNodes)
+
+        clone.id = template.id
+        container.appendChild(clone)
+        clone_app_name.textContent = Object.keys(cool_list)[i]
+
+        console.log(cool_list[Object.keys(cool_list)[i]][0])
+        clone_app_download.setAttribute("href", cool_list[Object.keys(cool_list)[i]][0])
+        clone_app_download.setAttribute("download", cool_list[Object.keys(cool_list)[i]][1])
+        clone_app_img.setAttribute("src", cool_list[Object.keys(cool_list)[i]][2])
+    }
+
+    template.style.display = "none"
+
+
+    // clone.id = clone.id //+ toString(clone_amount)
+    // console.log(clone_amount)
+
+    // container.appendChild(clone)
+    // clone_app_name.textContent = "adfg"
+};
 
 document.getElementById('pass').addEventListener('submit', function(event) {
     event.preventDefault(); // Stop default form submission and page reload
@@ -18,9 +63,7 @@ document.getElementById('pass').addEventListener('submit', function(event) {
     const password = "dimakrutoi"
 
     if (pinValue == password) {
-        alert("краава");
-
-        vse.style.display = "flex"
+        successful();
     } else {
         alert("хаахаха лоъ");
     }
