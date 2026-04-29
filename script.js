6d0cb184c479759fd5dfab79bbde512db6533e65
DelphanPAN <89398821+dolbaebsans@users.noreply.github.com>
2026-04-29 10:54:24 +0900

    Update script.js

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ let cool_list = {
     иван3: ["Сжатая архивная папка.rar", "ivan.png", "ivan.png"],
     иван4: ["Сжатая архивная папка.rar", "ivan.png", "ivan.png"],
     иван5: ["Сжатая архивная папка.rar", "ivan.png", "ivan.png"],
-    иван6: ["https://drive.google.com/file/d/1Vt2lvPcyY5uEkmVXb0cx6tTgFG0bAY9W/view?usp=drivesdk", "ivan.png", "ivan.png"],
+    иван6: ["https://drive.google.com/drive/folders/1oVtGk4bv4ZGIY4C2JaoEjVwPq_oVb7xo?usp=sharing", "ivan.png", "ivan.png"],
 }
 
 const pass_enter = document.getElementById("pass")
